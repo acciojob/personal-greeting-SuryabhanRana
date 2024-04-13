cf0ceb742d7a18eb05a3db053d6a600f0b5c3eb5
Suryabhan Rana <108150478+SuryabhanRana@users.noreply.github.com>
2024-04-13 15:10:14 +0000

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React,{ useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <form onSubmit={handleFormSubmit}>
+        <label>
+          Enter your name:
+          <br/>
+          <input
+            type="text"
+            value={name}
+            onChange={handleInputChange}
+          />
+        </label>
+      </form>
+      <p>{name && `Hello, ${name}!`}</p>
     </div>
   )
 }
